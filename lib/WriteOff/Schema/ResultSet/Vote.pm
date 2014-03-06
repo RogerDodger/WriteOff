617d0d314b643d09adca910f96f6278ed7375ba2
RogerDodger <roger.dodger.thor@gmail.com>
2014-03-06 15:37:30 +1030

    cleanup

--- lib/WriteOff/Schema/ResultSet/Vote.pm
+++ lib/WriteOff/Schema/ResultSet/Vote.pm
@@ -3,45 +3,16 @@ package WriteOff::Schema::ResultSet::Vote;
 use strict;
 use base 'WriteOff::Schema::ResultSet';
 
-=head2 average
-
-Returns the average score of a set of votes.
-
-Returns -2**31 if there are no votes in the resultset.
-
-While returning undef would be more mathematically correct, undef equals 0 in a
-numeric sort, which would give undesirable results.
-
-Also, returning undef would make the defined-or persistence in the image/story
-rows not work.
-
-=cut
-
 sub mean {
-	my $self = shift->search({ value => { '!=' => undef } });
-
-	return $self->{__mean} //=
-		$self->count == 0
-			? -(1 << 31)
-			: $self->get_column('value')->func('avg');
+	return shift->get_column('value')->func('avg');
 }
 
 sub average {
 	return shift->mean;
 }
 
-=head2 stdev
-
-Returns the standard deviation of a set of votes.
-
-Returns 0 if there are no votes in the resultset.
-
-=cut
-
 sub stdev {
-	my $self = shift;
-
-	return $self->get_column('value')->func('stdev');
+	return shift->get_column('value')->func('stdev');
 }
 
 sub prelim {
