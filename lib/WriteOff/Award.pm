8339686c21d3027d004a9c786893d111c703d3cb
RogerDodger <roger.dodger.thor@gmail.com>
2014-07-30 08:10:28 +0930

    Added Award.pm and award.t

---
+++ lib/WriteOff/Award.pm
@@ -0,0 +1,78 @@
+package WriteOff::Award;
+
+use v5.14;
+use warnings;
+use Carp;
+use Exporter;
+
+my @awards;
+
+BEGIN {
+	@awards = qw/GOLD SILVER BRONZE CONFETTI SPOON RIBBON/;
+
+	my $i = 0;
+	for my $award (@awards) {
+		$i++;
+		eval qq{
+			use constant _$award => $i;
+			sub $award () {
+				return __PACKAGE__->new(_$award);
+			}
+		};
+	}
+}
+
+our @ISA = qw/Exporter/;
+our @EXPORT_OK = ( @awards, qw/sort_awards/ );
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+my %attr = (
+	_GOLD()     => [ 'gold',     'Gold medal' ],
+	_SILVER()   => [ 'silver',   'Silver medal' ],
+	_BRONZE()   => [ 'bronze',   'Bronze medal' ],
+	_CONFETTI() => [ 'confetti', 'Most controversial' ],
+	_SPOON()    => [ 'spoon',    'Wooden spoon' ],
+	_RIBBON()   => [ 'ribbon',   'Participation ribbon' ],
+);
+
+sub new {
+	my ($class, $id) = @_;
+
+	unless (exists $attr{$id}) {
+		Carp::croak "Invalid award ID: $id";
+	}
+
+	return bless \$id, $class;
+}
+
+sub id {
+	return ${ +shift };
+}
+
+sub alt {
+	return $attr{shift->id}->[1];
+}
+
+sub name {
+	return $attr{shift->id}->[0];
+}
+
+sub src {
+	return '/static/images/awards/' . shift->name . '.png';
+}
+
+*type = \&name;
+
+sub sort_awards {
+	state $order = [ _GOLD, _SILVER, _BRONZE, _CONFETTI, _SPOON, _RIBBON ];
+
+	my %bin;
+	for my $award (@_) {
+		$bin{$award->id}++;
+	}
+
+	my @awards = map { WriteOff::Award->new($_) }
+	               map { ($_) x ($bin{$_} // 0) }
+	                  @$order;
+	return @awards;
+}
---
+++ t/award.t
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use v5.14;
+use strict;
+use warnings;
+use Test::More;
+use Test::Deep;
+
+BEGIN {
+	use_ok 'WriteOff::Award';
+	WriteOff::Award->import(':all');
+}
+
+my $award = GOLD;
+
+isa_ok $award, 'WriteOff::Award';
+
+is $award->id, 1, 'Award attributes';
+is $award->name, 'gold', 'Award attributes';
+is $award->type, 'gold', 'Award attributes';
+is $award->alt, 'Gold medal', 'Award attributes';
+is $award->src, '/static/images/awards/gold.png', 'Award attributes';
+
+cmp_deeply(
+	[
+		sort_awards GOLD, BRONZE, GOLD, RIBBON, SILVER,
+		CONFETTI, RIBBON, CONFETTI, SPOON, SPOON, GOLD, CONFETTI,
+	],
+	[
+		GOLD, GOLD, GOLD, SILVER, BRONZE, CONFETTI,
+		CONFETTI, CONFETTI, SPOON, SPOON, RIBBON, RIBBON,
+	],
+	'Awards sort correctly',
+);
