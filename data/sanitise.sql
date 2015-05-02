dc0f3e0733859e0d8f51dade4d5be469699cec97
RogerDodger <cthor@cpan.org>
2015-05-03 02:00:01 +1000

    update TODO and add sanitise.sql

---
+++ data/sanitise.sql
@@ -0,0 +1,17 @@
+-- Removes confidential data from the database
+-- such that it is suitable to give out publicly
+
+UPDATE users SET
+	-- "hunter2"
+	password = '$2$10$6F2eyWuzX1DmtnRsrDkU0uOa7PeedmRNO9TlVCCADtF9RIZD0ecwu',
+	email = NULL,
+	ip = NULL;
+
+DELETE FROM tokens;
+DELETE FROM bans;
+
+UPDATE storys SET ip = NULL, user_id = NULL;
+UPDATE images SET ip = NULL, user_id = NULL;
+UPDATE prompts SET ip = NULL, user_id = NULL;
+UPDATE vote_records SET ip = NULL, user_id = NULL;
+UPDATE artists SET user_id = NULL;
