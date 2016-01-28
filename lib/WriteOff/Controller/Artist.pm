2f38a70b14c2c89a450cd32854182b7c1dbdda2b
RogerDodger <roger.dodger.thor@gmail.com>
2016-01-29 09:07:19 +1030

    artist add template/controller

--- lib/WriteOff/Controller/Artist.pm
+++ lib/WriteOff/Controller/Artist.pm
@@ -1,13 +1,38 @@
 package WriteOff::Controller::Artist;
 use Moose;
 use namespace::autoclean;
-use Scalar::Util qw/looks_like_number/;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
 __PACKAGE__->config(model => 'Artist');
 
 sub fetch :Chained('/') :PathPart('artist') :CaptureArgs(1) :ActionClass('~Fetch') {}
+
+sub add :Local {
+	my ($self, $c) = @_;
+
+	$c->user or $c->detach('/forbidden', [ $c->{loginRequired} ]);
+
+	$c->forward('do_add') if $c->req->method eq 'POST';
+
+	$c->stash->{template} = 'artist/add.tt';
+	push $c->stash->{title}, $c->string('newArtistTitle');
+}
+
+sub do_add :Private {
+	my ($self, $c) = @_;
+
+	$c->forward('/check_csrf_token');
+
+	my $name = $c->req->param('artist');
+	if (1 <= length $name && length $name <= $c->config->{len}{max}{user}) {
+		$c->user->update({
+			active_artist => $c->user->create_related('artists', { name => $name })
+		});
+
+		$c->res->redirect($c->req->param('referer') || $c->uri_for('/'));
+	}
+}
 
 sub scores :Chained('fetch') :PathPart('scores') :Args(0) {
 	my ( $self, $c ) = @_;
@@ -16,53 +41,16 @@ sub scores :Chained('fetch') :PathPart('scores') :Args(0) {
 	$s->{genre_id} = $1 if $c->req->param('genre') =~ /^(\d+)/;
 	$s->{format_id} = $1 if $c->req->param('format') =~ /^(\d+)/;
 
-	$c->stash->{scores} = $c->stash->{artist}->entrys->search($s, {
+	$c->stash->{scores} = $c->stash->{artist}->scores->search($s, {
 		prefetch => 'event',
 		order_by => [
-			{ -desc  => 'event.created' },
-			{ -desc => 'score' },
+			{ -desc  => 'event.end' },
+			{ -desc => 'value' },
 		]
 	});
 
-	$c->stash->{theorys} = $c->stash->{artist}->theorys->search(
-		{
-			%$s,
-			award_id => { "!=" => undef },
-		},
-		{
-			prefetch => 'event',
-			order_by => { -desc => 'event.created' },
-		}
-	);
-
-	$c->stash->{scoreKey} = $s->{format_id} ? 'score_format' : 'score_genre';
-
 	$c->stash->{title} = 'Score Breakdown for ' . $c->stash->{artist}->name;
 	$c->stash->{template} = 'scoreboard/scores.tt';
-}
-
-sub swap :Local {
-	my ($self, $c) = @_;
-
-	$c->detach('/404') unless $c->req->method eq 'POST';
-
-	$c->forward('/check_csrf_token');
-
-	my $id = $c->req->param('artist-swap');
-	return unless looks_like_number $id;
-
-	if (my $artist = $c->user->artists->find($id)) {
-		$c->user->update({ active_artist_id => $artist->id });
-		if ($c->stash->{ajax}) {
-			$c->res->body('Okay');
-		}
-		else {
-			$c->res->redirect($c->req->referer);
-		}
-	}
-	else {
-		$c->detach('/error');
-	}
 }
 
 =head1 AUTHOR
