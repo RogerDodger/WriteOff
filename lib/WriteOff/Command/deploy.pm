9b4e4d2d09aa198331f9f357b56eca5dac42af33
RogerDodger <roger.dodger.thor@gmail.com>
2016-05-19 15:56:25 +0930

    add default data to deploy script

--- lib/WriteOff/Command/deploy.pm
+++ lib/WriteOff/Command/deploy.pm
@@ -1,5 +1,6 @@
 package WriteOff::Command::deploy;
 
+use DateTime;
 use WriteOff::Command;
 use WriteOff::Schema;
 use File::Spec;
@@ -32,7 +33,9 @@ sub run {
 
 	my $write_db = sub {
 		say "+ create $dbfn";
-		WriteOff::Schema->connect("dbi:SQLite:$dbfn")->deploy;
+		my $sch = WriteOff::Schema->connect("dbi:SQLite:$dbfn");
+		$sch->deploy;
+		$sch;
 	};
 
 	if (-e $dbfn) {
@@ -41,7 +44,31 @@ sub run {
 		if ($ans =~ $yes) {
 			say "- unlink $dbfn";
 			unlink $dbfn;
-			$write_db->();
+			my $sch = $write_db->();
+
+			$sch->resultset('EmailTrigger')->populate([
+				[qw/name template prompt_in_subject/],
+				['eventCreated', 'email/event-created.tt', 0],
+				['promptSelected', 'email/prompt-selected.tt', 0],
+				['votingStarted', 'email/voting-started.tt', 1],
+				['resultsUp', 'email/results-up.tt', 1],
+			]);
+
+			$sch->resultset('Format')->populate([
+				[qw/name wc_min wc_max/],
+				['Short Story', 2000, 8000],
+				['Minific', 400, 750],
+			]);
+
+			$sch->resultset('Genre')->populate([
+				[qw/name descr/],
+				['Original', 'Fiction not dependent on work under U.S. copyright'],
+			]);
+
+			$sch->resultset('Artist')->populate([
+				[qw/id name/],
+				[25, 'Anonymous'],
+			]);
 		}
 	}
 	else {
