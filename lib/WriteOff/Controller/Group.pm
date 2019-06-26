851d30a108acd6aecabe8811c680111fa5fe98b0
RogerDodger <roger.dodger.thor@gmail.com>
2019-06-26 12:08:10 +0930

    basic group single view

--- lib/WriteOff/Controller/Group.pm
+++ lib/WriteOff/Controller/Group.pm
@@ -11,7 +11,7 @@ sub _fetch :ActionClass('~Fetch') {}
 sub fetch :Chained('/') :PathPart('group') :CaptureArgs(1) {
    my ($self, $c) = @_;
    $c->forward('_fetch');
-   $c->stash->{group} = $c->stash->{genre};
+   $c->stash->{group} = $c->model('DB::Genre')->with_counts->find($c->stash->{genre}->id);
    $c->title_unshift($c->stash->{group}->name);
 }
 
@@ -33,6 +33,18 @@ sub index :Path('/groups') :Args(0) {
 }
 
 sub join :Chained('fetch') :PathPart('join') :Args(1) {
+
+}
+
+sub schedule :Chained('fetch') :PathPart('schedule') :Args(0) {
+
+}
+
+sub scoreboard :Chained('fetch') :PathPart('scoreboard') :Args(0) {
+
+}
+
+sub view :Chained('fetch') :PathPart('') :Args(0) {
 
 }
 
